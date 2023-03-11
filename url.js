fa1b699b712a08747234672e919877d0a6ef37a5
yu573957302 <126442722+yu573957302@users.noreply.github.com>
2023-03-11 20:47:12 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,12 +8,10 @@
 
 
 const urls = [
-	'https://forms.gle/CmX9XHUY7dxLKM3N7',
 	'https://forms.gle/wKaFHQiWg1urgDeZ8',
 	'https://forms.gle/1evizJW3y9DhQf5e8',
 	'https://forms.gle/LSDArovkRfia34Z28',
 	'https://forms.gle/YiYuUpaoLsvrWZMG8',
 	'https://forms.gle/CzDWheQx3FjQfPw8A',
 	'https://forms.gle/MttZ2c4vkx4WUKzq8',
-	'https://forms.gle/AvNc3BNxqDu8ZPG3A'
 ];
